cbd241de07adb8d53a54cfd757500e9228fcd752
Erick Gannem <erickgannem@gmail.com>
2020-09-30 22:41:34 -0400

    set up development environment / set up bootstrap

---
+++ src/App/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+
+function App() {
+  return (
+    <Container fluid style={{ padding: 0 }}>
+      <Navbar bg="dark" />
+    </Container>
+  );
+}
+
+export default App;
