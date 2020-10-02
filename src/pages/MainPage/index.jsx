618eeeeca1ecab5c9b028c46ab48ff5817950b7b
Erick Gannem <erickgannem@gmail.com>
2020-10-01 20:46:00 -0400

    separate item as component

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,51 +1,18 @@
 import React from 'react';
 
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
-
 import Container from 'react-bootstrap/Container';
+
+import CItem from '../../components/CItem';
 
 function MainPage() {
   return (
     <Container fluid>
       <Row style={{ justifyContent: 'center', marginTop: 15 }}>
-        <Card text="dark" className="m-4">
-          <Card.Header>
-            Title
-          </Card.Header>
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card text="dark" className="m-4">
-          <Card.Header>
-            Title
-          </Card.Header>
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card text="dark" className="m-4">
-          <Card.Header>
-            Title
-          </Card.Header>
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+        <CItem />
+        <CItem />
+        <CItem />
+
       </Row>
 
     </Container>
