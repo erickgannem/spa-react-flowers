627764af433c85ac18db2077e4f4e651d47286da
Erick Gannem <erickgannem@gmail.com>
2020-10-02 09:29:16 -0400

    setup wouter routes

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import './index.css';
 
 import CNavbar from '../CNavbar';
-import MainPage from '../../pages/MainPage';
 
 function App() {
   return (
     <>
       <CNavbar name="El Jardin de Mamá" bg="light" />
-      <MainPage />
     </>
 
   );
--- src/components/CNavbar/index.jsx
+++ src/components/CNavbar/index.jsx
@@ -4,40 +4,54 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 
-import { Link, Route } from 'wouter';
+import { Link } from 'wouter';
+
+import Routes from '../Routes';
 
 function CNavbar(props) {
   const { bg, name } = props;
   return (
-    <Navbar bg={bg} style={{ justifyContent: 'space-between' }} expand="md">
-      <Navbar.Brand href="#home">{name}</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <>
+      <Navbar bg={bg} style={{ justifyContent: 'space-between' }} expand="md">
+        <Navbar.Brand href="#home">
+          <Link href="/">
+            {name}
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-      <Navbar.Collapse style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
-        <Nav>
-          <Nav.Item className="p-2">
-            <Nav.Link className="nav-link" href="#">
-              <Link href="/ofertas">Ofertas</Link>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item className="p-2">
-            <Nav.Link className="nav-link" href="#">
-              <Link href="/macetas">Macetas</Link>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item className="p-2">
-            <Nav.Link className="nav-link" href="#"><Link href="/accesorios">Accesorios</Link></Nav.Link>
-          </Nav.Item>
-          <Nav.Item className="p-2">
-            <Nav.Link className="nav-link" href="#"><Link href="/herramientas">Herramientas</Link></Nav.Link>
-          </Nav.Item>
-          <Nav.Item className="p-2">
-            <Button variant="outline-primary">Entrar</Button>
-          </Nav.Item>
-        </Nav>
-      </Navbar.Collapse>
+        <Navbar.Collapse style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+          <Nav>
+            <Nav.Item className="p-2">
+              <Nav.Link className="nav-link" href="#">
+                <Link href="/ofertas">Ofertas</Link>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="p-2">
+              <Nav.Link className="nav-link" href="#">
+                <Link href="/macetas">Macetas</Link>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="p-2">
+              <Nav.Link className="nav-link" href="#"><Link href="/accesorios">Accesorios</Link></Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="p-2">
+              <Nav.Link className="nav-link" href="#"><Link href="/herramientas">Herramientas</Link></Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="p-2">
+              <Button variant="outline-primary">
+                <Link href="/entrar">
+                  Entrar
+                </Link>
+              </Button>
 
-    </Navbar>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+
+      </Navbar>
+      <Routes />
+    </>
   );
 }
 CNavbar.defaultProps = {
