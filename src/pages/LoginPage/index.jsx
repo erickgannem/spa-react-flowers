55795a8c16544a6ce9abcf09fc0d971733f58946
Erick Gannem <erickgannem@gmail.com>
2020-10-02 16:39:46 -0400

    created login page structure

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import './index.css';
 
 function LoginPage() {
+  const [, setUsername] = useState('');
+  const [, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div><h1>Login</h1></div>
+    <Row noGutters className="justify-content-center align-items-center" style={{ marginTop: 60 }}>
+      <Col lg="auto" sm="auto" xs="auto">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="username">
+            <Form.Label>Usuario</Form.Label>
+            <Form.Control type="text" size="lg" onChange={(e) => setUsername(e.target.value)} />
+          </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control type="password" size="lg" onChange={(e) => setPassword(e.target.value)} />
+          </Form.Group>
+          <Form.Group controlId="controls" className="d-flex flex-column">
+            <Button variant="success" type="submit">Entrar</Button>
+            <Form.Text className="text-center">¿Olvidó su contraseña?</Form.Text>
+
+          </Form.Group>
+
+        </Form>
+      </Col>
+    </Row>
+
   );
 }
 
