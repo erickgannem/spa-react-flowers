9739e3daa6b0a71879aebd46af605c929ec8c457
Erick Gannem <erickgannem@gmail.com>
2020-10-06 22:12:08 -0400

    create feedback reducer

---
+++ src/reducers/feedbackReducer.js
@@ -0,0 +1,26 @@
+export default function feedbackReducer(state, action) {
+  switch (action.type) {
+    case 'SET_LOADING':
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case 'UNSET_LOADING':
+      return {
+        ...state,
+        isLoading: false,
+      };
+    case 'SET_ERROR':
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    case 'CLEAR_ERROR':
+      return {
+        ...state,
+        error: {},
+      };
+    default:
+      return state;
+  }
+}
