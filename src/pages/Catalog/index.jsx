c2f5cd3030915e811e18017eb62eb36992b908dd
Erick Gannem <erickgannem@gmail.com>
2020-10-09 11:53:45 -0400

    Refactor catalog code

--- src/pages/Catalog/index.jsx
+++ src/pages/Catalog/index.jsx
@@ -6,34 +6,8 @@ import {
 
 import CatalogItem from '../../components/CatalogItem';
 
-// dummy data
-const DUMMY_ITEMS = [
-  { name: 'Planta #1', available: false },
-  { name: 'Planta #2', available: true },
-  { name: 'Planta #3', available: true },
-  { name: 'Planta #4', available: true },
-  { name: 'Planta #5', available: false },
-  { name: 'Planta #6', available: false },
-  { name: 'Planta #7', available: true },
-  { name: 'Planta #8', available: true },
-  { name: 'Planta #9', available: true },
-  { name: 'Planta #10', available: true },
-  { name: 'Planta #11', available: true },
-  { name: 'Planta #12', available: false },
-  { name: 'Planta #13', available: true },
-  { name: 'Planta #14', available: true },
-  { name: 'Planta #15', available: false },
-  { name: 'Planta #16', available: false },
-  { name: 'Planta #17', available: false },
-  { name: 'Planta #18', available: false },
-  { name: 'Planta #19', available: false },
-  { name: 'Planta #20', available: false },
-  { name: 'Planta #21', available: false },
-  { name: 'Planta #22', available: false },
-];
-
 const totalPages = (itemsArr) => {
-  const itemsCount = itemsArr.length;
+  const itemsCount = itemsArr ? itemsArr.length : 1;
   return Math.ceil(itemsCount / 3);
 };
 
@@ -72,12 +46,12 @@ function Catalog() {
     }
     return items;
   }, [activePage]);
-
-  const items = setPaginationBoxes(totalPages(DUMMY_ITEMS));
+  const items = setPaginationBoxes(totalPages());
 
   return (
     <>
       <Row noGutters className="justify-content-center align-items-center">
+
         <Col xl={3} lg={3} md={3}>
           <CatalogItem
             key={1}
@@ -85,9 +59,12 @@ function Catalog() {
             available
           />
         </Col>
+
       </Row>
+
       <Row noGutters className="justify-content-center align-items-center">
         <Col xl={3} lg={3} md={3}>
+
           <Pagination size="sm" className="justify-content-center align-items-center">
             <Pagination.Prev onClick={() => activePage > 1 && setActivePage(activePage - 1)} />
             {items}
