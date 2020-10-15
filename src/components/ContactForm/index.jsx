e1ad7ecc4e236ca55cd4d74b57f90554f2872903
Erick Gannem <erickgannem@gmail.com>
2020-10-15 11:01:19 -0400

    Style footer

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -6,21 +6,21 @@ import './index.css';
 
 function ContactForm() {
   return (
-    <Container>
+    <Container className="my-0">
       <Row>
         <Col>
           <Form>
-            <Form.Text as="h4">
+            <Form.Text as="h4" className="mt-0 mb-3">
               Formulario de Contacto
             </Form.Text>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+            <Form.Group>
+              <Form.Control type="email" placeholder="Email" />
             </Form.Group>
-
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+            <Form.Group>
+              <Form.Control type="text" placeholder="Nombre" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control type="textarea" placeholder="Comentario" />
             </Form.Group>
             <Button variant="primary" type="submit">
               Enviar
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { IoIosCall, IoMdMailOpen } from 'react-icons/io';
+import { FaMapMarkerAlt } from 'react-icons/fa';
+
 import './index.css';
 
 import { Container, Col, Row } from 'react-bootstrap';
@@ -7,23 +10,31 @@ import ContactForm from '../ContactForm';
 
 function Footer() {
   return (
-    <Container className="app-footer py-4" fluid>
+    <Container className="app-footer py-3" fluid>
       <Row>
-        <Col xs={6} md={6} lg={10}>
-          <h4>Contáctanos</h4>
-          <span>
-            Telefono
-          </span>
-          <br />
-          <span>
-            Email
-          </span>
-          <br />
-          <span>
-            Dirección
-          </span>
+        <Col xs={12} md={6} lg={4} className="justify-content-center align-items-center">
+          <h4 className="mt-0 mb-3">Contáctanos</h4>
+          <Row className="my-1">
+            <Col>
+              <IoIosCall className="mr-1" />
+              Telefono
+            </Col>
+          </Row>
+          <Row className="my-1">
+            <Col>
+              <IoMdMailOpen className="mr-1" />
+              Email
+            </Col>
+          </Row>
+          <Row className="my-1">
+            <Col>
+              <FaMapMarkerAlt className="mr-1" />
+              Dirección
+            </Col>
+          </Row>
+
         </Col>
-        <Col xs={6} md={6} lg={2}>
+        <Col xs={12} md={6} lg={3} className="justify-content-center align-items-center px-0">
           <ContactForm />
         </Col>
       </Row>
