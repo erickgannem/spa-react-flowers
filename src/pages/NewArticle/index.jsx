8e45278f5f714fc3699e66d34078172695f82892
Erick Gannem <erickgannem@gmail.com>
2020-10-19 14:17:17 -0400

    style new article form

--- src/pages/NewArticle/index.jsx
+++ src/pages/NewArticle/index.jsx
@@ -3,28 +3,37 @@ import {
   Container, Row, Col, Form,
 } from 'react-bootstrap';
 
+import './index.css';
+
 function NewArticle() {
   const nameInput = React.createRef();
   const priceInput = React.createRef();
   const descriptionInput = React.createRef();
 
   return (
-    <Form>
-      <Form.Group>
-        <Form.Label>Nombre de la planta</Form.Label>
-        <Form.Control type="text" ref={nameInput} />
-      </Form.Group>
+    <Container>
+      <Row>
+        <Col>
+          <Form className="p-5 d-flex flex-column app-new-article-form">
+            <Form.Group>
+              <Form.Label>Nombre de la planta</Form.Label>
+              <Form.Control type="text" ref={nameInput} />
+            </Form.Group>
 
-      <Form.Group>
-        <Form.Label>Precio</Form.Label>
-        <Form.Control type="text" ref={priceInput} />
-      </Form.Group>
+            <Form.Group>
+              <Form.Label>Precio</Form.Label>
+              <Form.Control type="text" ref={priceInput} />
+            </Form.Group>
 
-      <Form.Group>
-        <Form.Label>Descripción</Form.Label>
-        <Form.Control type="text" ref={descriptionInput} />
-      </Form.Group>
-    </Form>
+            <Form.Group>
+              <Form.Label>Descripción</Form.Label>
+              <Form.Control type="text" ref={descriptionInput} />
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+
   );
 }
 
