7d77f6bb73887e8efa9db28d5d78d99976e317ef
Erick Gannem <erickgannem@gmail.com>
2020-10-22 14:24:36 -0400

    Add video player to about page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -4,20 +4,29 @@ import './index.css';
 import {
   Container, Image, Row, Col,
 } from 'react-bootstrap';
-import VideoPlayer from '../../components/VideoPlayer';
+
+import YouTube from 'react-youtube';
 
 import config from '../../shopConfig';
 
 function About() {
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
   const {
-    videoUrl, description1, description2, description3, photo1, photo2,
+    videoId, description1, description2, description3, photo1, photo2,
   } = config.aboutPage;
   return (
-    <Container fluid className="d-flex flex-column justify-content-start h-100 my-5">
+    <Container fluid className="d-flex flex-column justify-content-start my-5">
       <Row className="video-section">
-        <Col xs={12} className="d-flex justify-content-center">
-          <h1>Video WIP</h1>
-          {/* <VideoPlayer videoUrl={videoUrl} /> */}
+        <Col className="d-flex justify-content-center">
+          <YouTube videoId={videoId} opts={opts} />
         </Col>
       </Row>
       <Row className="description-one-section">
@@ -26,18 +35,18 @@ function About() {
         </Col>
       </Row>
       <Row className="description-2-photo-section">
-        <Col md={6} className="d-flex justify-content-center align-items-center">
+        <Col className="d-flex justify-content-center align-items-center">
           <p>{description2}</p>
         </Col>
-        <Col md={6} className="d-flex justify-content-center">
+        <Col className="d-flex justify-content-center">
           <Image src={photo1} style={{ width: 200 }} />
         </Col>
       </Row>
       <Row className="description-3-photo-section">
-        <Col md={6} className="d-flex justify-content-center">
+        <Col className="d-flex justify-content-center">
           <Image src={photo2} style={{ width: 200 }} />
         </Col>
-        <Col md={6} className="d-flex justify-content-center align-items-center">
+        <Col className="d-flex justify-content-center align-items-center">
           <p>{description3}</p>
         </Col>
       </Row>
--- src/shopConfig.js
+++ src/shopConfig.js
@@ -5,7 +5,7 @@ export default {
   email: 'jardin@gmail.com',
 
   aboutPage: {
-    videoUrl: 'https://www.youtube.com/watch?v=lxbH51TxoBk&ab_channel=PLANTAS%2CFLORESYANIMALESSINCENSURA',
+    videoId: 'lxbH51TxoBk',
     history: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Final de texto",
 
     description1: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. ",
