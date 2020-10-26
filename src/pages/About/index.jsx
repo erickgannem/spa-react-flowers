9985ad0827d83978ef7d956828ba6c70a942da2c
Erick Gannem <erickgannem@gmail.com>
2020-10-25 20:36:31 -0400

    fix about page styling

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -14,7 +14,6 @@ function About() {
     height: '390',
     width: '640',
     playerVars: {
-      // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
     },
   };
@@ -23,31 +22,31 @@ function About() {
     videoId, description1, description2, description3, photo1, photo2,
   } = config.aboutPage;
   return (
-    <Container fluid className="d-flex flex-column justify-content-start my-5">
+    <Container fluid className="d-flex flex-column justify-content-start my-5 px-5">
       <Row className="video-section">
-        <Col className="d-flex justify-content-center">
-          <YouTube videoId={videoId} opts={opts} />
+        <Col className="embed-responsive embed-responsive-4by3">
+          <YouTube videoId={videoId} opts={opts} className="embed-responsive-item" />
         </Col>
       </Row>
-      <Row className="description-one-section">
+      <Row className="description-one-section my-5">
         <Col xs={12}>
-          <p>{description1}</p>
+          <p className="description">{description1}</p>
         </Col>
       </Row>
       <Row className="description-2-photo-section">
-        <Col className="d-flex justify-content-center align-items-center">
-          <p>{description2}</p>
+        <Col className="d-flex justify-content-center align-items-start my-3" lg={10}>
+          <p className="description">{description2}</p>
         </Col>
-        <Col className="d-flex justify-content-center">
-          <Image src={photo1} style={{ width: 200 }} />
+        <Col className="d-flex justify-content-center justify-content-lg-end my-3" lg={2}>
+          <Image fluid src={photo1} />
         </Col>
       </Row>
-      <Row className="description-3-photo-section">
-        <Col className="d-flex justify-content-center">
-          <Image src={photo2} style={{ width: 200 }} />
+      <Row className="description-3-photo-section my-5">
+        <Col className="d-flex justify-content-center justify-content-lg-start my-2" lg={2}>
+          <Image fluid src={photo2} />
         </Col>
-        <Col className="d-flex justify-content-center align-items-center">
-          <p>{description3}</p>
+        <Col className="d-flex justify-content-center align-items-start my-3" lg={10}>
+          <p className="description">{description3}</p>
         </Col>
       </Row>
     </Container>
